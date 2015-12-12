7526095e5ab9c087c31cff52269de9d979d7f397
Vincent Tatan <vincentt.2013@sis.smu.edu.sg>
2015-12-12 08:48:04 +0800

    Build a project that uses Semantic UI (CSS and Javascript) for templates and experiment

--- VSMSRenewed/web/js/main.js
+++ VSMSRenewed/web/js/main.js
@@ -1,7 +1,85 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
+
+    // Hide Sign Up side on initialization
+    $('.inactive').hide();
+
+    $('.mini.button.signup').click(function() {
+
+        // Hide Sign In and show Sign Up side with slide down effect
+        $('.ui.segment.signin')
+                .hide()
+                .end()
+                .find('.ui.segment.signup')
+                .slideDown();
+
+    });
 
 
+    $('.mini.button.signin').click(function() {
+
+        // Hide Sign Up and show Sign In side with slide down effect
+        $('.ui.segment.signup')
+                .hide()
+                .end()
+                .find('.ui.segment.signin')
+                .slideDown();
+
+    });
+});
+
+$('.ui.form')
+        .form({
+            username: {
+                identifier: 'username',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter a username'
+                    }
+                ]
+            },
+            email: {
+                identifier: 'email',
+                rules: [
+                    {
+                        type: 'email',
+                        prompt: 'Please enter a valid email addres'
+                    }
+                ]
+            },
+            password: {
+                identifier: 'password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please enter a password'
+                    },
+                    {
+                        type: 'length[6]',
+                        prompt: 'Your password must be at least 6 characters'
+                    }
+                ]
+            },
+            passwordConfirm: {
+                identifier: 'confirm-password',
+                rules: [
+                    {
+                        type: 'empty',
+                        prompt: 'Please confirm your password'
+                    },
+                    {
+                        type: 'match[password]',
+                        prompt: 'Password doesn\'t match'
+                    }
+                ]
+            },
+            terms: {
+                identifier: 'terms',
+                rules: [
+                    {
+                        type: 'checked',
+                        prompt: 'You must agree to the terms and conditions'
+                    }
+                ]
+            }
+        });
