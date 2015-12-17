b6c2223e1c372942f9fc3596f8c3f17271b27728
David <David@192.168.1.165>
2015-12-18 01:52:45 +0800

    By Bowei--------------------------------------------------------------------------------------------------- 1.Completed IngredientDAO 2.Tests to be done at a later time

--- VSMSRenewed/src/java/DAO/IngredientDAO.java
+++ VSMSRenewed/src/java/DAO/IngredientDAO.java
@@ -6,10 +6,15 @@
 package DAO;
 
 import Controller.ConnectionManager;
+import Model.Dish;
 import Model.Ingredient;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *
@@ -210,5 +215,419 @@ public class IngredientDAO {
                 }                
             }
         }
+    }
+    
+    public ArrayList<Dish> getDish(String vendor_id){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        ArrayList<Dish> dishList=new ArrayList<Dish>();
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "select * from Dish where vendor_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, vendor_id);
+            rs = statement.executeQuery();
+            while(rs.next()){
+               int dishId=Integer.parseInt(rs.getString("dish_id"));
+               int venId=Integer.parseInt(rs.getString("vendor_id"));
+               dishList.add(new Dish(dishId,rs.getString("dish_name"),venId, rs.getString("dish_description"), getIngredientQuantity(rs.getString("dish_id"), vendor_id)));   
+            }
+            
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        return dishList;
+    }
+        
+    public HashMap<Ingredient, ArrayList<String>> getIngredientQuantity(String dish_id, String vendor_id) {
+        HashMap<Ingredient, ArrayList<String>> toReturn = new HashMap<Ingredient, ArrayList<String>>();
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+
+        try {
+            conn = ConnectionManager.getConnection();
+            query = "select * from IngredientQuantity where dish_id=? & vendor_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, dish_id);
+            statement.setString(2, vendor_id);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                ArrayList<String> tempList = new ArrayList<String>();
+                tempList.add(rs.getString("quantity"));
+                tempList.add(rs.getString("unit"));
+                toReturn.put(this.getIngredient(rs.getString("supplier_id"), rs.getString("ingredient_name")), tempList);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return toReturn;
+    }
+    
+    public void addDish(Dish dish){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        
+        
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "insert into Dish (dish_id, dish_name, vendor_id, dish_description) values (?,?,?,?)";
+            statement = conn.prepareStatement(query);
+            statement.setString(1,dish.getDish_id()+"");
+            statement.setString(2,dish.getDish_name());
+            statement.setString(3,dish.getVendor_id()+"");
+            statement.setString(4,dish.getDish_description());
+            int rows = statement.executeUpdate();
+            HashMap<Ingredient, ArrayList<String>> map=dish.getIngredientQuantity();
+            Set<Ingredient> iSet=map.keySet();
+            Iterator<Ingredient> iter=iSet.iterator();
+            while(iter.hasNext()){
+                Ingredient tempI=iter.next();
+                ArrayList<String> tempList=map.get(tempI);
+                this.addIngredientQuantity(tempI, tempList.get(0), tempList.get(1), dish.getDish_id()+"", dish.getVendor_id()+"");
+            }
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        
+    }
+    
+    public void addIngredientQuantity(Ingredient ingredient,String quantity, String unit, String dish_id, String vendor_id){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query ="insert into IngredientQuantity (dish_id, ingredient_name, vendor_id, supplier_id, quantity, unit) values (?,?,?,?,?,?)";
+            statement = conn.prepareStatement(query);
+            statement.setString(1,dish_id);
+            statement.setString(2,ingredient.getName());
+            statement.setString(3,vendor_id);
+            statement.setString(4,ingredient.getSupplier_id()+"");
+            statement.setString(5, quantity);
+            statement.setString(6, unit);
+            int rows = statement.executeUpdate();
+           
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        
+    }
+    
+    public void deleteDish(Dish dish){
+        HashMap<Ingredient, ArrayList<String>> map=dish.getIngredientQuantity();
+        Set<Ingredient> iSet = map.keySet();
+        Iterator<Ingredient> iter = iSet.iterator();
+        while (iter.hasNext()) {
+            Ingredient tempI = iter.next();
+            ArrayList<String> tempList = map.get(tempI);
+            this.deleteIngredientQuantity(dish.getDish_id()+"", tempI.getName(), dish.getVendor_id()+"", tempI.getSupplier_id() + "");
+        }
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        
+        
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "Delete from Dish where dish_id=? & vendor_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, dish.getDish_id()+"");
+            statement.setString(2, dish.getVendor_id()+"");
+            int row = statement.executeUpdate();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        
+        
+    }
+    
+    public void deleteIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "Delete from IngredientQuantity where dish_id=? & ingredient_name=? & vendor_id=? & supplier_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, dish_id);
+            statement.setString(2, ingredient_name);
+            statement.setString(3, vendor_id);
+            statement.setString(4, supplier_id);
+            int row = statement.executeUpdate();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        
+    }
+    
+    public void updateDish(Dish dish){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        
+        
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "update Dish set dish_id=?, dish_name=?, vendor_id=?, dish_description=? where dish_id=? & vendor_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, dish.getDish_id()+"");
+            statement.setString(2, dish.getDish_name());
+            statement.setString(3, dish.getVendor_id()+"");
+            statement.setString(4, dish.getDish_description());
+            statement.setString(5, dish.getDish_id()+"");
+            statement.setString(3, dish.getVendor_id()+"");
+            int row = statement.executeUpdate();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        HashMap<Ingredient, ArrayList<String>> map=dish.getIngredientQuantity();
+        Set<Ingredient> iSet = map.keySet();
+        Iterator<Ingredient> iter = iSet.iterator();
+        while (iter.hasNext()) {
+            Ingredient tempI = iter.next();
+            ArrayList<String> tempList = map.get(tempI);
+            this.updateIngredientQuantity(dish.getDish_id()+"", tempI.getName(), dish.getVendor_id()+"", tempI.getSupplier_id() + "",tempList.get(0),tempList.get(1));
+        }
+    }
+    
+    public void updateIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id, String quantity, String unit){
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "";
+        try
+        {
+            conn=ConnectionManager.getConnection();
+            query = "update IngredientQuantity set dish_id=?, ingredient_name=?, vendor_id=?, supplier_id=?, quantity=?, unit=? where dish_id=? & ingredient_name=? & vendor_id=? & supplier_id=?";
+            statement = conn.prepareStatement(query);
+            statement.setString(1, dish_id);
+            statement.setString(2, ingredient_name);
+            statement.setString(3, vendor_id);
+            statement.setString(4, supplier_id);
+            statement.setString(5, quantity);
+            statement.setString(6, unit);
+            statement.setString(7, dish_id);
+            statement.setString(8, ingredient_name);
+            statement.setString(9, vendor_id);
+            statement.setString(10, supplier_id);
+            int row = statement.executeUpdate();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if(statement != null)
+            {
+                try
+                {
+                    statement.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null)
+            {
+                try
+                {
+                    conn.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }                
+            }
+        }
+        
     }
 }
