4f4f94a890a3493f54401860e4d8a0645c939e59
Benjamin <Benjamin@Benjamin-PC>
2015-12-17 22:02:22 +0800

    Edited Order class and OrderDAO

--- VSMSRenewed/src/java/Model/Order.java
+++ VSMSRenewed/src/java/Model/Order.java
@@ -5,37 +5,37 @@ package Model;
  * @author Joel
  */
 public class Order {
-    private String order_id;
-    private String vendor_id;
-    private int total_final_price;
+    private int order_id;
+    private int vendor_id;
+    private double total_final_price;
     
-    public Order(String order_id, String vendor_id, int total_final_price){
+    public Order(int order_id, int vendor_id, double total_final_price){
         this.order_id = order_id;
         this.vendor_id = vendor_id;
         this.total_final_price = total_final_price;
     }
     
-    public String getOrder_id(){
+    public int getOrder_id(){
         return order_id;
     }
     
-    public String getVendor_id(){
+    public int getVendor_id(){
         return vendor_id;
     }
     
-    public int getTotal_final_price(){
+    public double getTotal_final_price(){
         return total_final_price;
     }
     
-    public void setOrder_id(String order_id){
+    public void setOrder_id(int order_id){
         this.order_id = order_id;
     }
     
-    public void setVendor_id(String vendor_id){
+    public void setVendor_id(int vendor_id){
         this.vendor_id = vendor_id;
     }
     
-    public void setTotal_final_price(int total_final_price){
+    public void setTotal_final_price(double total_final_price){
         this.total_final_price = total_final_price;
     }
     
