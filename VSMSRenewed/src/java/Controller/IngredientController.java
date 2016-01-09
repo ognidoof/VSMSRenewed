ef4d7e813a5ad0cec874d9e89e9abc9f7047ca72
TC <TC@10.124.130.104>
2016-01-09 15:48:32 +0800

    IngredientController 	- Converted methods to be static

--- VSMSRenewed/src/java/Controller/IngredientController.java
+++ VSMSRenewed/src/java/Controller/IngredientController.java
@@ -17,53 +17,53 @@ import java.util.HashMap;
  */
 public class IngredientController {
     
-    IngredientDAO iDAO = new IngredientDAO();
+    static IngredientDAO iDAO = new IngredientDAO();
     
-    public Ingredient getIngredient(String supplierId, String ingredientName){
+    public static Ingredient getIngredient(String supplierId, String ingredientName){
         return iDAO.getIngredient(supplierId, ingredientName);
     }
     
-    public int addIngredient(Ingredient ingredient){
+    public static int addIngredient(Ingredient ingredient){
         return iDAO.addIngredient(ingredient);
     }
     
-    public void deleteIngredient(Ingredient ingredient){
+    public static void deleteIngredient(Ingredient ingredient){
         iDAO.deleteIngredient(ingredient);
     }
     
-    public void updateIngredient(Ingredient ingredient){
+    public static void updateIngredient(Ingredient ingredient){
         iDAO.updateIngredient(ingredient);
     }
     
-    public ArrayList<Dish> getDish(String vendor_id){
+    public static ArrayList<Dish> getDish(String vendor_id){
         return iDAO.getDish(vendor_id);
     }
     
-    public HashMap<Ingredient, ArrayList<String>> getIngredientQuantity(String dish_id, String vendor_id) {
+    public static HashMap<Ingredient, ArrayList<String>> getIngredientQuantity(String dish_id, String vendor_id) {
         return iDAO.getIngredientQuantity(dish_id, vendor_id);
     }
     
-    public void addDish(Dish dish){
+    public static void addDish(Dish dish){
         iDAO.addDish(dish);
     }
     
-    public void addIngredientQuantity(Ingredient ingredient,String quantity, String unit, String dish_id, String vendor_id){
+    public static void addIngredientQuantity(Ingredient ingredient,String quantity, String unit, String dish_id, String vendor_id){
         iDAO.addIngredientQuantity(ingredient, quantity, unit, dish_id, vendor_id);
     }
     
-    public void deleteDish(Dish dish){
+    public static void deleteDish(Dish dish){
         iDAO.deleteDish(dish);
     }
     
-    public void deleteIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id){
+    public static void deleteIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id){
         iDAO.deleteIngredientQuantity(dish_id, ingredient_name, vendor_id, supplier_id);
     }
     
-    public void updateDish(Dish dish){
+    public static void updateDish(Dish dish){
         iDAO.updateDish(dish);
     }
     
-    public void updateIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id, String quantity, String unit){
+    public static void updateIngredientQuantity(String dish_id, String ingredient_name, String vendor_id, String supplier_id, String quantity, String unit){
         iDAO.updateIngredientQuantity(dish_id, ingredient_name, vendor_id, supplier_id, quantity, unit);
     }
 }
