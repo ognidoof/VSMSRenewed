42f948dfd9cea9400430c42fe796b8de02da3dc7
Benjamin_2 <Benjamin_2@10.124.128.73>
2016-01-09 11:42:15 +0800

    Updated OrderDAO to include methods for Orderline

--- VSMSRenewed/src/java/DAO/OrderDAO.java
+++ VSMSRenewed/src/java/DAO/OrderDAO.java
@@ -63,4 +63,39 @@ public class OrderDAO {
         return orderList;
     }
     
+    public static ArrayList<Orderline> retrieveOrderLineList(int vendor_id, int order_id ) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "";
+        ArrayList<Order> orderList = new ArrayList<Order>();
+        try {
+            //creates connections to database
+            conn = ConnectionManager.getConnection();
+            sql = "SELECT * FROM `orderline` WHERE vendor_id = #1 && order_id = #2";
+            sql = sql.replace("#1", ""+vendor_id);
+            sql = sql.replace("#2", ""+order_id);
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            //Retrieves the supplier info from database and create a new supplier object to return
+            while (rs.next()) {
+                
+//                int vendor_id = rs.getInt("vendor_id");
+//                int order_id = rs.getInt("order_id");
+                int supplier_id = rs.getInt("supplier_id");
+                
+                double total_final_price = rs.getDouble("total_final_price");
+
+                Order order = new Order(order_id, vendor_id ,total_final_price);
+                orderList.add(order);
+            }
+        } catch (SQLException e) {
+            handleSQLException(e, sql);
+        } finally {
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        return orderList;
+    }
+    
 }
