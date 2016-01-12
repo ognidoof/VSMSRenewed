b8f5e2a78fea20ce1b7646e4ca33d6a638903678
Joel <Joel@10.124.130.47>
2016-01-12 14:31:49 +0800

    Updated OrderDAO to be able to update orders

--- VSMSRenewed/src/java/DAO/OrderDAO.java
+++ VSMSRenewed/src/java/DAO/OrderDAO.java
@@ -237,4 +237,55 @@ public class OrderDAO {
         }
     }
 
+    public void updateOrder(Order order){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "";
+        try{
+            conn = ConnectionManager.getConnection();
+            sql = "UPDATE orders"
+                    + "SET total_final_price = #1"
+                        + "WHERE order_id=#2 && vendor_id=#3";
+            sql = sql.replace("#1", "" + order.getTotal_final_price());
+            sql = sql.replace("#2", "" + order.getOrder_id());
+            sql = sql.replace("#3", "" + order.getVendor_id());
+            stmt = conn.prepareStatement(sql);
+            stmt.executeUpdate();
+            
+            ArrayList<Orderline> orderLineList = order.getOrderlines();
+            updateOrderlines(orderLineList);
+            
+        }catch(SQLException e){
+            handleSQLException(e, sql);
+        }finally{
+            ConnectionManager.close(conn, stmt, rs);
+        }
+    }
+    
+    public void updateOrderlines(ArrayList<Orderline> orderlines){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "";
+        
+        for(Orderline newOrderline : orderlines){
+            try{
+                conn = ConnectionManager.getConnection();
+                sql = "UPDATE orderline"
+                        + "SET ingredient_name = #1 , price = #2 , quantity = #3 , buffer_percentage = #4"
+                            + "WHERE vendor_id = #5 && order_id = #6";
+                sql = sql.replace("#1", newOrderline.getIngredient_name());
+                sql = sql.replace("#2", "" + newOrderline.getFinalprice());
+                sql = sql.replace("#3", "" + newOrderline.getQuantity());
+                sql = sql.replace("#4", "" + newOrderline.getBufferpercentage());
+                sql = sql.replace("#5", "" + newOrderline.getVendor_id());
+                sql = sql.replace("#6", "" + newOrderline.getSupplier_id());
+            }catch(SQLException e){
+            handleSQLException(e, sql);
+            }finally{
+            ConnectionManager.close(conn, stmt, rs);
+        }
+        }
+    }
 }
